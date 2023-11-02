a073662543443fb5c8986687dbe5b994b5fbce2d
Prog <progrodv@gmail.com>
2023-11-02 21:56:54 +0300

    Custom card.

--- lib/features/receipt_list/widgets/receipt_list_item.dart
+++ lib/features/receipt_list/widgets/receipt_list_item.dart
@@ -10,13 +10,22 @@ class ReceiptListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 160,
-      child: Card(
-        margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
         child: Container(
-          color: const Color(0xFFFFFFFF),
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            boxShadow: [
+              BoxShadow(
+                color: Color(0x19929292),
+                spreadRadius: 2,
+                blurRadius: 4,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image(
                 image: AssetImage(model.imageLocation),
@@ -39,7 +48,7 @@ class ReceiptListItem extends StatelessWidget {
                           style: const TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.w500,
-                            height: 0.95,
+                            height: 1,
                           ),
                         ),
                         const SizedBox(height: 12),
@@ -64,6 +73,68 @@ class ReceiptListItem extends StatelessWidget {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget build2(BuildContext context) {
+    return SizedBox(
+      height: 160,
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 1,
+        shadowColor: const Color(0xCFF4F4F4),
+        color: const Color(0xFFFFFFFF),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: AssetImage(model.imageLocation),
+              height: 136,
+              width: 149,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 30, 23, 23),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        model.title,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        softWrap: false,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          height: 0.95,
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      Row(
+                        children: [
+                          const Icon(Icons.access_time, size: 16),
+                          const SizedBox(width: 11),
+                          Text(
+                            '${model.cookingTimeMinutes} минут',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF2ECC71),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
