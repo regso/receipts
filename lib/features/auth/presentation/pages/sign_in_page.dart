4cdd8d246b0282256c8688fd21de6645aac9cde1
regso <55665811+regso@users.noreply.github.com>
2024-03-22 19:12:43 +0300

    Refactor sign in page.

--- lib/features/auth/presentation/pages/sign_in_page.dart
+++ lib/features/auth/presentation/pages/sign_in_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receipts/config/app_theme.dart';
-import 'package:receipts/config/labels.dart';
 import 'package:receipts/config/routes/app_page.dart';
-import 'package:receipts/config/routes/app_router_delegate.dart';
 import 'package:receipts/features/app/presentation/bloc/app_bloc.dart';
 import 'package:receipts/features/app/presentation/bloc/app_state.dart';
 import 'package:receipts/features/auth/presentation/widgets/sign_in_form.dart';
@@ -18,30 +16,18 @@ class SignInPage extends StatelessWidget {
       builder: (BuildContext context, AppState state) {
         return Scaffold(
           backgroundColor: AppTheme.accentColor,
-          body: SafeArea(
-            child: Center(
-              child: Column(
-                children: [
-                  const Expanded(
-                    child: Center(
-                      child: SizedBox(
-                        width: 232,
-                        child: SignInForm(),
-                      ),
+          body: const SafeArea(
+            child: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    SizedBox(height: 100),
+                    SizedBox(
+                      width: 232,
+                      child: SignInForm(),
                     ),
-                  ),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      foregroundColor: AppTheme.inverseTextColor,
-                      padding: const EdgeInsets.all(20),
-                    ),
-                    child: Text(Labels.authSignInRegistration),
-                    onPressed: () {
-                      (Router.of(context).routerDelegate as AppRouterDelegate)
-                          .open(pageSlug: AppPageSlug.authSignUp);
-                    },
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
